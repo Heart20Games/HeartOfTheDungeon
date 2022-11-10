f9580b1126aa87a803bb68958d39ae9b49b2d9d4
Matthew <Music.matthew.wood@gmail.com>
2022-11-10 12:44:26 -0500

    Wwise SoundbackRelocated

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,15 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_CLUB_SWING_VAR = 2698480971U;
+        static const AkUniqueID PLAY_DAGGER_SWING_VAR = 345029485U;
+        static const AkUniqueID PLAY_DUNGEONDOOKKICK = 655318279U;
         static const AkUniqueID PLAY_PLAYER_FOOTSTEPS = 98439365U;
         static const AkUniqueID PLAY_PLAYER_JUMP = 562256996U;
         static const AkUniqueID PLAY_PLAYER_LAND = 4249207015U;
+        static const AkUniqueID PLAY_SWORD_BLOCK_VAR = 1256834993U;
+        static const AkUniqueID PLAY_SWORD_HIT_VAR = 105369445U;
+        static const AkUniqueID PLAY_SWORD_SWING_VAR = 2181790556U;
         static const AkUniqueID PLAY_TESTMUSIC_1_LP = 850545534U;
         static const AkUniqueID PLAY_TESTMUSIC_2_LP = 3902639735U;
         static const AkUniqueID PLAY_TESTSOUND1 = 1958090956U;
@@ -25,6 +31,22 @@ namespace AK
         static const AkUniqueID PLAY_TESTSOUND2_LP_STOP = 946131661U;
         static const AkUniqueID PLAY_TESTSOUND2_STOP = 4060986362U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace VOPLAYING
+        {
+            static const AkUniqueID GROUP = 2267245470U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID VOISNOTPLAYING = 147619015U;
+                static const AkUniqueID VOISPLAYING = 73236054U;
+            } // namespace STATE
+        } // namespace VOPLAYING
+
+    } // namespace STATES
 
     namespace GAME_PARAMETERS
     {
@@ -56,7 +78,7 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID TESTSOUNDBANK = 1831431028U;
+        static const AkUniqueID PLAYERSOUNDS = 1327972334U;
     } // namespace BANKS
 
     namespace BUSSES
@@ -66,12 +88,29 @@ namespace AK
         static const AkUniqueID _3DAMBIENCE = 1301074112U;
         static const AkUniqueID AMBIENTBEDS = 1182634443U;
         static const AkUniqueID AMBIENTMASTER = 1459460693U;
+        static const AkUniqueID BAELORB = 916373224U;
+        static const AkUniqueID CLAVICULUSC = 3353640785U;
+        static const AkUniqueID GOBLIN = 2995046728U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID NPCMASTER = 2033911932U;
+        static const AkUniqueID OSSEUS = 709208847U;
+        static const AkUniqueID OSTEON = 2352081845U;
+        static const AkUniqueID PLAYER_COMBAT = 676956834U;
         static const AkUniqueID PLAYERCLOTH = 765206498U;
         static const AkUniqueID PLAYERFOOTSTEPS = 1681012287U;
         static const AkUniqueID PLAYERLOCOMOTION = 2343802269U;
         static const AkUniqueID PLAYERMASTER = 3538689948U;
+        static const AkUniqueID RATGUARDS = 663048024U;
+        static const AkUniqueID ROTTA = 1480202049U;
+        static const AkUniqueID SCENEAMBIENTFOLEY = 2599435418U;
+        static const AkUniqueID SCENECHARACTERFOLEY = 3920882365U;
+        static const AkUniqueID SCENEFOLEY = 348626796U;
+        static const AkUniqueID SCENESPECIALFOLEY = 1997697613U;
+        static const AkUniqueID THEHERO = 1870928622U;
+        static const AkUniqueID VENEL = 3602628327U;
+        static const AkUniqueID VERPFEIFEN = 237722175U;
+        static const AkUniqueID VOICEBUS = 2045367873U;
     } // namespace BUSSES
 
     namespace AUX_BUSSES
