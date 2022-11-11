b7f2cfd519b4821e7970b68a782c031f6524b995
Matthew <Music.matthew.wood@gmail.com>
2022-11-10 21:37:26 -0500

    TestSoundOnPlay

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,118 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_DUNGEONDOOKKICK = 655318279U;
+        static const AkUniqueID PLAY_TEST_KA_CHING = 2872110659U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace VOPLAYING
+        {
+            static const AkUniqueID GROUP = 2267245470U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID VOISNOTPLAYING = 147619015U;
+                static const AkUniqueID VOISPLAYING = 73236054U;
+            } // namespace STATE
+        } // namespace VOPLAYING
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DENSITY_FA_SSGRAIN = 2715217995U;
+        static const AkUniqueID IGN_ENG_EXH_POSITION = 1776289502U;
+        static const AkUniqueID IGN_GEARBOX_THROTTLE = 3744760799U;
+        static const AkUniqueID IGN_HARLEY_GRAIN_LOOP_BLEND = 1573260114U;
+        static const AkUniqueID IGN_LOAD = 1046277832U;
+        static const AkUniqueID IGN_RPM = 2202868459U;
+        static const AkUniqueID IGNITERSYNTH_REV = 1355046805U;
+        static const AkUniqueID IGNITERSYNTH_STATE = 3696491809U;
+        static const AkUniqueID IMMERSION_FA_SSGRAIN = 2481728872U;
+        static const AkUniqueID INSTRUMENT_FA_SSGRAIN = 2317409760U;
+        static const AkUniqueID PROXIMITY_FA_SSGRAIN = 1791284502U;
+        static const AkUniqueID RPM_FA_SSGRAIN = 1656280998U;
+        static const AkUniqueID SIMULATION_FA_SSGRAIN = 2428833394U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK2 = 1647770722U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID _2DAMBIENCE = 309309195U;
+        static const AkUniqueID _2DAMBIENTBEDS = 4152869693U;
+        static const AkUniqueID _3DAMBIENCE = 1301074112U;
+        static const AkUniqueID AMBIENTBEDS = 1182634443U;
+        static const AkUniqueID AMBIENTMASTER = 1459460693U;
+        static const AkUniqueID BAELORB = 916373224U;
+        static const AkUniqueID CLAVICULUSC = 3353640785U;
+        static const AkUniqueID GOBLIN = 2995046728U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID NPCMASTER = 2033911932U;
+        static const AkUniqueID OSSEUS = 709208847U;
+        static const AkUniqueID OSTEON = 2352081845U;
+        static const AkUniqueID PLAYER_COMBAT = 676956834U;
+        static const AkUniqueID PLAYERCLOTH = 765206498U;
+        static const AkUniqueID PLAYERFOOTSTEPS = 1681012287U;
+        static const AkUniqueID PLAYERLOCOMOTION = 2343802269U;
+        static const AkUniqueID PLAYERMASTER = 3538689948U;
+        static const AkUniqueID RATGUARDS = 663048024U;
+        static const AkUniqueID ROTTA = 1480202049U;
+        static const AkUniqueID SCENEAMBIENTFOLEY = 2599435418U;
+        static const AkUniqueID SCENECHARACTERFOLEY = 3920882365U;
+        static const AkUniqueID SCENEFOLEY = 348626796U;
+        static const AkUniqueID SCENESPECIALFOLEY = 1997697613U;
+        static const AkUniqueID THEHERO = 1870928622U;
+        static const AkUniqueID VENEL = 3602628327U;
+        static const AkUniqueID VERPFEIFEN = 237722175U;
+        static const AkUniqueID VOICEBUS = 2045367873U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CAVE = 4122393694U;
+        static const AkUniqueID HALLWAY = 2198133169U;
+        static const AkUniqueID LARGEROOM = 187046019U;
+        static const AkUniqueID REVERBS = 3545700988U;
+        static const AkUniqueID SMALLROOM = 2933838247U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
