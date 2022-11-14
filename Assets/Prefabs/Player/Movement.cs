d3ada2be6b87e71356584b65a752520a55cf07da
Zachary <zacharytjones1@gmail.com>
2022-11-13 20:10:32 -0800

    Initial movement working, moving in direction intended

--- Assets/Prefabs/Player/Movement.cs
+++ Assets/Prefabs/Player/Movement.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 800f;
+
+    private Vector2 MovementVector = new Vector2(0,0);
+
+    private Rigidbody myRigidbody;
+
+    PlayerControls controls;
+
+    private void Awake()
     {
+        myRigidbody = GetComponent<Rigidbody>();
+        controls = new PlayerControls();
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // this.transform.local
         
+        //myRigidbody.AddForce(new Vector3(MovementVector.x, 0, MovementVector.y) * speed * Time.deltaTime, ForceMode.Force); // deltatime decouples the framerate from movement.
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidbody.AddRelativeForce(new Vector3(MovementVector.x, 0, MovementVector.y) * speed * Time.deltaTime, ForceMode.Force);
+    }
+
+    public void ReceiveInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Movement input");
+            Debug.Log(context.valueType);
+            Debug.Log(context.ReadValue<Vector2>());
+
+            Vector2 inputVector = context.ReadValue<Vector2>();
+            // nowthe actual movement logic
+            // myRigidbody.AddForce(new Vector3(inputVector.x, 0, inputVector.y)* speed, ForceMode.Force);
+            // time to hack it.
+            MovementVector = inputVector;
+        }
+        //horizontalInput = input;
+        //print(horizontalInput);
     }
 }
