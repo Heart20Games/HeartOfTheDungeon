6b121fc75977a4e0374e0312b2b293a1dbc06209
Zachary <zacharytjones1@gmail.com>
2022-11-17 15:53:40 -0800

    I fixed my whoopsiedoodle.  I accidentally killed good code with bad practices.

--- Assets/Prefabs/Player/PlayerCore.cs
+++ Assets/Prefabs/Player/PlayerCore.cs
@@ -7,7 +7,6 @@ using Yarn.Unity;
 
 public class PlayerCore : MonoBehaviour
 {
-    
     public bool talkable = false;
     public string targetNode = "";
 
@@ -19,48 +18,64 @@ public class PlayerCore : MonoBehaviour
 
     private void Start()
     {
-        dialogueRunner = dialogueHolder.GetComponent<DialogueRunner>();
-        dialogueRunner.onDialogueComplete.AddListener(DoneTalking);
+
         moveControls = GetComponent<Movement>();
+
+        if (dialogueHolder != null)
+        {
+            dialogueRunner = dialogueHolder.GetComponent<DialogueRunner>();
+            dialogueRunner.onDialogueComplete.AddListener(DoneTalking);
+        }
     }
     // For keeping track of things like health and other instance specific things.
     // Stat block here
-    public void Update()
-    {
-        /* if(moveControls.getMoveVector().x > 0.5f)
-        {
-            East = true;
-        }else if(moveControls.getMoveVector().x < -0.5f)
-        {
-            East = false;
-        }else if()
-        if(moveControls.getMoveVector().y > 0.5f)
-        {
-            North = true;
-        } */
 
-    }
 
     // Public methods here
     public void Die()
     {
         Debug.Log("You are Dead");
         SceneManager.LoadScene("GameOver"); // Whisks us directly to the game over screen.
-
     }
 
     public void Talk()
     {
-        Debug.Log("I'm talking now");
-        if (talkable)
+        if (talkable && dialogueRunner != null)
         {
-            dialogueRunner.StartDialogue(targetNode);
-            gameObject.GetComponent<Movement>().canMove = false;
+            if (targetNode != "")
+            {
+                dialogueRunner.Stop();
+                Debug.Log("I'm talking now");
+                dialogueRunner.StartDialogue(targetNode);
+                gameObject.GetComponent<Movement>().canMove = false;
+            }
+            else
+            {
+                Debug.LogWarning("No target node");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No Dialogue Runner to Start Talking");
         }
     }
 
     private void DoneTalking()
     {
+        Debug.Log("I'm done talking now");
         gameObject.GetComponent<Movement>().canMove = true;
+    }
+
+    [YarnCommand("enter_room")]
+    private void EnterRoom(string roomName)
+    {
+        if (SceneUtility.GetBuildIndexByScenePath(roomName) >= 0)
+        {
+            SceneManager.LoadScene(roomName);
+        }
+        else
+        {
+            Debug.LogWarning("Scene " + roomName + " not in build.");
+        }
     }
 }
