d68a547b072126abb987bd6c90665b5e9b5a6185
Ian Cullum <cullumi@wwu.edu>
2022-11-20 07:50:01 -0800

    Proper enemy walking, weapon damage, toggleable enemy sounds

--- Assets/Prefabs/Characters/Enemies/EnemyType.cs
+++ Assets/Prefabs/Characters/Enemies/EnemyType.cs
@@ -19,21 +19,33 @@ public class EnemyType : ScriptableObject
     public string hurtSoundUrl = "";
     public string fightSoundUrl = "";
     public string walkSoundUrl = "";
+
+    public bool useHurtSound = false;
+    public bool useFightSound = false;
+    public bool useWalkSound = false;
+
     public FMOD.Studio.EventInstance hurtSound;
     public FMOD.Studio.EventInstance fightSound;
     public FMOD.Studio.EventInstance walkSound;
     
     public void Initialize()
     {
-        hurtSound = InitializeSound("Hurt", hurtSoundUrl);
-        fightSound = InitializeSound("Fight", fightSoundUrl);
-        walkSound = InitializeSound("Walk", walkSoundUrl);
+        hurtSound = InitializeSound("Hurt", useHurtSound, hurtSoundUrl);
+        fightSound = InitializeSound("Fight", useFightSound, fightSoundUrl);
+        walkSound = InitializeSound("Walk", useWalkSound, walkSoundUrl);
     }
 
-    public FMOD.Studio.EventInstance InitializeSound(string soundType, string url)
+    public FMOD.Studio.EventInstance InitializeSound(string soundType, bool use, string url)
     {
-        FMOD.Studio.EventInstance sound = FMODUnity.RuntimeManager.CreateInstance(url);
-        if (!hurtSound.isValid()) { Debug.LogWarning(soundType + " Sound not Valid: " + enemyName); }
-        return sound;
+        if (use)
+        {
+            FMOD.Studio.EventInstance sound = FMODUnity.RuntimeManager.CreateInstance(url);
+            if (!hurtSound.isValid()) { Debug.LogWarning(soundType + " Sound not Valid: " + enemyName); }
+            return sound;
+        }
+        else
+        {
+            return new FMOD.Studio.EventInstance();
+        }
     }
 }
