cd0113908f2e60f79bbb5794a071a1f2496f9dcc
Ian Cullum <cullumi@wwu.edu>
2023-01-06 02:20:08 -0800

    Fixed Start Game button not loading correct Scene
    + StartGame Component now takes an InMemryVariableStorage component to access Scenes by Room name.

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Yarn.Unity;
 
 public class StartGame : MonoBehaviour
 {
-    [SerializeField] private string targetScene;
+    public InMemoryVariableStorage storage;
+    [SerializeField] public string targetRoom = "$Hub";
+
     public void StartGameplay()
     {
-        
         print("Game starts here!");
-        SceneManager.LoadScene(targetScene);
+        if (storage != null)
+        {
+            string targetScene = targetRoom;
+            storage.TryGetValue<string>(targetRoom, out targetScene);
+            SceneManager.LoadScene(targetScene);
+        }
+        else
+        {
+            Debug.LogWarning("Cannot find InMemoryVariableStorage componenent.");
+            SceneManager.LoadScene(targetRoom);
+        }
     }
 }
