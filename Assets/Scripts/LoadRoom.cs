a8589647b32fa45c7101451bf5fea3c10657efe3
Ian Cullum <cullumi@wwu.edu>
2023-01-06 02:39:24 -0800

    Improved Warnings from LoadRoom.cs

--- Assets/Scripts/LoadRoom.cs
+++ Assets/Scripts/LoadRoom.cs
@@ -1,1 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
+
 public class LoadRoom : MonoBehaviour
+{
+    public InMemoryVariableStorage storage;
+    [SerializeField] public string targetRoom = "$Hub";
+
+    public void StartGameplay()
+    {
+        print("Game starts here!");
+        if (targetRoom.StartsWith("$") && storage != null)
+        {
+            string targetScene;
+            if (storage.TryGetValue<string>(targetRoom, out targetScene))
+            {
+                SceneManager.LoadScene(targetScene);
+            }
+            else
+            {
+                Debug.LogWarning("Can't find room " + targetRoom + ", using as Scene name instead.");
+                SceneManager.LoadScene(targetRoom);
+            }
+        }
+        else
+        {
+            if (storage == null)
+            {
+                Debug.LogWarning("Cannot find InMemoryVariableStorage componenent.");
+            }
+            SceneManager.LoadScene(targetRoom);
+        }
+    }
+}
