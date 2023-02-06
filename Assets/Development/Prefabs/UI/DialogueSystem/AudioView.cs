700b70a8eb9ed3b1f56a9b2495951ef53b514569
Ian Cullum <cullumi@wwu.edu>
2023-02-06 12:33:34 -0800

    Implemented AudioView for Yarn Spinner lines
    + Provides finite control of how sounds are handled
    + Supports a "skip" tag that defaults to the button noise. (#no-va)

--- Assets/Development/Prefabs/UI/DialogueSystem/AudioView.cs
+++ Assets/Development/Prefabs/UI/DialogueSystem/AudioView.cs
@@ -4,17 +4,61 @@ using Yarn.Unity;
 using UnityEngine.UI;
 using System;
 using System.Collections.Generic;
+using FMODUnity;
 
 public class AudioView : DialogueViewBase
 {
-
-    string lastCharacter = "";
-    bool leftNext = false;
+    public string skipTag = "no-va";
+    public EventReference fmodEvent;
+    public EventReference skipFmodEvent;
+    private FMOD.Studio.EventInstance eventInstance;
+    private FMOD.Studio.EventInstance skipInstance;
 
     //Action advanceHandler = null;
 
     public void Awake()
     {
+        eventInstance = RuntimeManager.CreateInstance(fmodEvent);
+        eventInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
+        skipInstance = RuntimeManager.CreateInstance(skipFmodEvent);
+        skipInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
+    }
+
+    private bool HasTag(string[] metaData, string tag)
+    {
+        if (metaData != null)
+        {
+            foreach (string meta in metaData)
+            {
+                if (meta == tag)
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    private void PlaySound(LocalizedLine dialogueLine)
+    {
+        bool skip = HasTag(dialogueLine.Metadata, skipTag);
+        FMOD.Studio.EventInstance instance = skip ? skipInstance : eventInstance;
+        FMOD.Studio.EventInstance other = skip ? eventInstance : skipInstance;
+
+        instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        other.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        string clean = dialogueLine.TextID.Remove(4, 1);
+        if (!skip)
+        {
+            Debug.Log("Play Line");
+            instance.setParameterByNameWithLabel("IntroVOParam", clean); // IntroVOParam is on the Intro Scene even (Fmod event)
+        }
+        else
+        {
+            Debug.Log("Skip Line");
+        }
+        instance.start();
     }
 
     public override void RunLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
@@ -25,8 +69,16 @@ public class AudioView : DialogueViewBase
             return;
         }
 
+        PlaySound(dialogueLine);
         onDialogueLineFinished();
         //base.RunLine(dialogueLine, onDialogueLineFinished);
+    }
+
+    public override void InterruptLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
+    {
+        PlaySound(dialogueLine);
+        onDialogueLineFinished();
+        //base.InterruptLine(dialogueLine, onDialogueLineFinished);
     }
 
     public override void DismissLine(Action onDismissalComplete)
@@ -37,6 +89,8 @@ public class AudioView : DialogueViewBase
             return;
         }
 
+        eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        //skipInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         onDismissalComplete();
         //base.DismissLine(onDismissalComplete);
     }
