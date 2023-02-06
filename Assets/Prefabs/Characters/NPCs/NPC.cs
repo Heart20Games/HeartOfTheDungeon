ad6627990c40a6fd2160e52c8d93e1891ebe2af5
Ian Cullum <cullumi@wwu.edu>
2023-02-06 05:07:56 -0800

    NPC now inherits Interactable

--- Assets/Prefabs/Characters/NPCs/NPC.cs
+++ Assets/Prefabs/Characters/NPCs/NPC.cs
@@ -2,18 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour
+public class NPC : Interactable
 {
-    public string yarnNode;
-    public PlayerCore player;
-
-    public void NotifyPlayerEnteredInteractable()
-    {
-        player.FoundTalkable(yarnNode);
-    }
-
-    public void NotifyPlayerLeftInteractable()
-    {
-        player.LeftTalkable(yarnNode);
-    }
 }
