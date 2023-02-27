87206e8c2a3a6b7a9dc344d6b0b7655bc637f6a5
Matthew <Music.matthew.wood@gmail.com>
2023-02-27 14:47:58 -0500

    Added Obstacle Sound Trigger Method

--- Assets/FmodEventReferences.cs
+++ Assets/FmodEventReferences.cs
@@ -11,7 +11,11 @@ public class FmodEventReferences : MonoBehaviour
     [field: SerializeField] public EventReference UISelectHigh { get; private set; }
     [field: SerializeField] public EventReference UIMenuSelect { get; private set; }
     [field: SerializeField] public EventReference UIMenuSwipe { get; private set; }
+    [field: Header("EnvironmentSounds")]
     [field: SerializeField] public EventReference FloorPlate { get; private set; }
+    [field: SerializeField] public EventReference SpikeTrap { get; private set; }
+    [field: SerializeField] public EventReference SpinWall { get; private set; }
+    [field: SerializeField] public EventReference GateOpen { get; private set; }
     public static FmodEventReferences instance { get; private set; }
 
     private void Awake()
