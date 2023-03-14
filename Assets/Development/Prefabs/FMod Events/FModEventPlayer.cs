2640d5d774864567137a60ccd455130b3124dbaa
Ian Cullum <cullumi@wwu.edu>
2023-03-13 19:39:01 -0700

    FModEvent System Implemented

---
+++ Assets/Development/Prefabs/FMod Events/FModEventPlayer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class FModEventPlayer : MonoBehaviour
+{
+    public FModEventLibary libary;
+    public Vector3 offset = Vector3.zero;
+    public Transform source;
+
+    private void Awake()
+    {
+        if (source == null)
+        {
+            source = gameObject.transform;
+        }
+    }
+
+    public void HelloWorld(string key)
+    {
+        Debug.Log("Hello world!");
+    }
+
+    public void PlayEvent(string key)
+    {
+        Vector3 worldPos = source.position + offset;
+        PlayEvent(key, worldPos);
+    }
+
+    public void PlayEvent(string key, Vector3 worldPos)
+    {
+        if (libary == null)
+        {
+            Debug.LogWarning("Can't find any FMod Event Libary!");
+        }
+        else
+        {
+            EventReference reference = libary.GetReference(key);
+            PlayOneShot(reference, worldPos);
+        }
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+
+    public EventInstance CreateInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        return eventInstance;
+    }
+}
