584c3c7e94aeb51f6621928e19898a503728b41e
JJArakus <Jeremiah@arakusband.com>
2023-03-24 19:02:17 -0700

    Spell Slot Functionality Implemented

---
+++ Assets/Resources/Art/UI/HUD/Spell Slots/SpellSlotTester.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellSlotTester : MonoBehaviour
+{
+    public int spellCost;
+    public int spellGain;
+    public int addSlots;
+    public bool callGain = false;
+    public bool callCost = false;
+    public bool addTotal = false;
+    public SpellSlots spellSlots;
+    
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(callCost)
+        {
+            spellSlots.SpendSpellSlots(spellCost);        
+        }
+
+        if(callGain)
+        {
+            spellSlots.GainSpellSlots(spellGain);            
+        }
+
+        if(addTotal)
+        {
+            spellSlots.IncreaseSpellSlots(addSlots);
+        }
+        
+        callCost = false;
+        callGain = false;
+        addTotal = false;
+    }
+
+
+}
