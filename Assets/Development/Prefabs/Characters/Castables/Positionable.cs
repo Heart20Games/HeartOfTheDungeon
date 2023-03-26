d1d3c4c88d932d66511f9a352edabd1701b9941f
Ian Cullum <cullumi@wwu.edu>
2023-03-26 15:00:33 -0700

    Fixed Projectiles sometimes not firing the right direction
    + also removed abstract from Positionable

---
+++ Assets/Development/Prefabs/Characters/Castables/Positionable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Positionable : MonoBehaviour, IPositionable
+{
+    public Transform source;
+    public Transform target;
+
+    public virtual void SetOrigin(Transform source, Transform target)
+    {
+        this.source = source;
+        this.target = target;
+    }
+
+    public virtual void MoveToOrigin()
+    {
+        MoveToOrigin(null);
+    }
+
+    public virtual void MoveToOrigin(Transform toMove)
+    {
+        toMove = toMove != null ? toMove : transform;
+        toMove.SetParent(source);
+        toMove.position = target.position;
+    }
+}
