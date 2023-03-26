2857c9805516feead1b32b753855e4c4f6e15677
Ian Cullum <cullumi@wwu.edu>
2023-03-26 15:00:33 -0700

    Fixed Projectiles sometimes not firing the right direction
    + also removed abstract from Positionable

--- Assets/Development/ExtensionMethods.cs
+++ Assets/Development/ExtensionMethods.cs
@@ -12,13 +12,9 @@ public static class ExtensionMethods
         return false;
     }
 
-    public static void SetRotationWithVector(this Transform _transform, Vector2 vector, float rotationOffset=0, float threshold=0.5f)
+    public static void SetRotationWithVector(this Transform _transform, Vector2 vector, float rotationOffset=0)
     {
-        Vector3 direction = Vector3.forward;
-        if (Mathf.Abs(vector.x) > threshold || Mathf.Abs(vector.y) > threshold)
-        {
-            direction = Vector3.right * vector.x + Vector3.forward * vector.y;
-        }
+        Vector3 direction = Vector3.right * vector.x + Vector3.forward * vector.y;
         if (direction.sqrMagnitude > 0.0f)
         {
             Quaternion newRotation = Quaternion.LookRotation(direction, Vector3.up);
--- Assets/Development/Prefabs/Characters/Castables/Weapons/Weapon.cs
+++ Assets/Development/Prefabs/Characters/Castables/Weapons/Weapon.cs
@@ -97,13 +97,6 @@ public class Weapon : Castable
         Vector2 dir = new Vector2(direction.x, direction.z);
         pInstance.SetRotationWithVector(dir, rotationOffset);
         bInstance.localRotation = Quaternion.identity;
-        //if (instanced)
-        //{
-        //    print("Location: " + pInstance.position);
-        //    print("Forward: " + pInstance.forward);
-        //    print("P Rotation: " + pInstance.rotation);
-        //    print("B Rotation: " + bInstance.rotation);
-        //}
     }
 
     public IEnumerator CleanupInstance(float lifeSpan, Transform pInstance, Transform bInstance)
