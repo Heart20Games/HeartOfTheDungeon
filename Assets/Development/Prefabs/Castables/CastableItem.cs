7e594abaaa88c97fac5e5ad6244c2b7f326b6f14
Ian Cullum <cullumi@wwu.edu>
2023-05-30 08:04:45 -0700

    Started CastableItem and abstracted Context vector data.

---
+++ Assets/Development/Prefabs/Castables/CastableItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CastableItem", menuName = "ScriptableObjects/CastableItem", order = 1)]
+public class CastableItem : ScriptableObject
+{
+
+}
