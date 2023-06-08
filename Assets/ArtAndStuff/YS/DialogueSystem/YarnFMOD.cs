bd5dbf8e6a00d39bed1d715c500b745391abd112
Ian Cullum <cullumi@wwu.edu>
2023-06-07 20:34:01 -0700

    Fixed YarnFMOD commands being reliant on editor-only code

--- Assets/ArtAndStuff/YS/DialogueSystem/YarnFMOD.cs
+++ Assets/ArtAndStuff/YS/DialogueSystem/YarnFMOD.cs
@@ -3,25 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
 using Yarn.Unity;
+using FMOD.Studio;
 
 public class YarnFMOD : BaseMonoBehaviour
 {
+    public FModEventLibary fmodEventLibrary;
     private StudioEventEmitter emitter;
+
     public void Start()
     {
         emitter = GetComponent<StudioEventEmitter>();
     }
 
-    [YarnCommand("url_emit")]
-    public void emitByURL(string url)
+    [YarnCommand("emit_sound")]
+    public void EmitByURL(string soundName)
     {
         emitter.Stop();
-        emitter.EventReference = EventReference.Find(url);
+        emitter.EventReference = fmodEventLibrary.GetReference(soundName);
         emitter.Play();
     }
 
-    [YarnCommand("stop_emit")]
-    public void stopEmit()
+    [YarnCommand("stop_sound")]
+    public void StopEmit()
     {
         emitter.Stop();
     }
