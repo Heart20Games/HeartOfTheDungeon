9343901f5102aeac3e61a08d00e630bed08e563e
Ian Cullum <ian.p.cullum@gmail.com>
2023-07-17 02:25:06 -0700

    Implemented back button for PanelOpener

--- Assets/Development/Prefabs/UI/PanelOpener.cs
+++ Assets/Development/Prefabs/UI/PanelOpener.cs
@@ -1,7 +1,6 @@
 using UnityEngine.EventSystems;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PanelOpener : MonoBehaviour
 {
@@ -9,30 +8,48 @@ public class PanelOpener : MonoBehaviour
 	public EventSystem eventSystem;
 	public GameObject previouslySelected;
 	public GameObject firstSelected;
+	public InputActionReference cancelAction;
 	public bool alwaysOpenToFirst = false;
-	private GameObject returnTo;
 
-	public void OpenPanel()
+    private GameObject returnTo;
+	private bool active = false;
+
+    public void Awake()
+    {
+        if (cancelAction.action != null)
+			if (!cancelAction.action.enabled)
+				cancelAction.action.Enable();
+    }
+
+    public void Update()
+    {
+        if (active && cancelAction.action != null && cancelAction.action.triggered)
+		{
+			OpenPanel();
+		}
+    }
+
+    public void OpenPanel()
 	{
 		if (Panel !=null)
 		{
-			bool isActive = Panel.activeSelf;
-			Panel.SetActive(!isActive);
+			active = !Panel.activeSelf;
+			Panel.SetActive(active);
 			if (Panel !=null) 
 			{
-			if (!isActive)
-			{ 
-				// In Open Panel method, when opening:
-previouslySelected = eventSystem.currentSelectedGameObject;
-eventSystem.SetSelectedGameObject((alwaysOpenToFirst || returnTo == null) ? firstSelected : returnTo);
-
-			} else 
-			{
-// In Open Panel method, when closing:
-returnTo = eventSystem.currentSelectedGameObject;
-eventSystem.SetSelectedGameObject(previouslySelected);
-previouslySelected = null;
-			}
+				if (active)
+				{
+					// In Open Panel method, when opening:
+					previouslySelected = eventSystem.currentSelectedGameObject;
+					eventSystem.SetSelectedGameObject((alwaysOpenToFirst || returnTo == null) ? firstSelected : returnTo);
+			    }
+                else 
+			    {
+                    // In Open Panel method, when closing:
+                    returnTo = eventSystem.currentSelectedGameObject;
+                    eventSystem.SetSelectedGameObject(previouslySelected);
+                    previouslySelected = null;
+			    }
 			}
 		}
 	}
