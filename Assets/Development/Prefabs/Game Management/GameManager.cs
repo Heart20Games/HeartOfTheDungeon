f9edb0b9da34d168f005c4b4acdfb77465150a73
Ian Cullum <ian.p.cullum@gmail.com>
2023-07-18 18:39:52 -0700

    Split GameManager into GameInput and Gamesettings.
    Also: GameMode swapping uses a ScriptableObject with a dictionary of structs.

--- Assets/Development/Prefabs/Game Management/GameManager.cs
+++ Assets/Development/Prefabs/Game Management/GameManager.cs
@@ -10,21 +10,13 @@ using System;
 
 public class Game : BaseMonoBehaviour
 {
-    [Serializable]
-    public struct GameSettings
-    {
-        public bool useD20Menu;
-    }
+    public static Game game;
 
     // Properties
     public Character playerCharacter;
     public List<Character> playableCharacters;
     public Selector selector;
-    public string characterInputMap = "GroundMovement";
-    public string selectorInputMap = "Selector";
-    public string dialogueInputMap = "Dialogue";
-    public string dismissInputMap = "Dismiss";
-    public GameSettings settings = new();
+    public GameSettings settings;
     [HideInInspector] public UserInterface userInterface;
     [HideInInspector] public HUD hud;
     [HideInInspector] public List<ITimeScalable> timeScalables;
@@ -34,17 +26,13 @@ public class Game : BaseMonoBehaviour
     private Character curCharacter;
     [HideInInspector] public Character CurCharacter { get { return curCharacter; } set { SetCharacter(value); } }
     private int curCharIdx = 0;
+    public int CurCharIdx { get => curCharIdx; }
     
     // TimeScale
     public float timeScale = 1.0f;
     public float TimeScale { get { return timeScale; } set { SetTimeScale(value); } }
-    
-    // Game Mode
-    public enum GameMode { Selection, Character, Dialogue, Dismiss };
-    private GameMode mode = GameMode.Character;
-    public GameMode Mode { get { return mode; } set { SetMode(value); } }
 
-    // Input
+    // Inputs
     private PlayerInput input;
 
     // Cheats / Shortcuts
@@ -53,17 +41,17 @@ public class Game : BaseMonoBehaviour
     // Events
     public UnityEvent onPlayerDied;
 
-    private void Awake()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
 
     // Initialization
 
+    private void Awake()
+    {
+        game = this;
+        input = GetComponent<PlayerInput>();
+    }
+
     private void Start()
     {
-        input = GetComponent<PlayerInput>();
         InitializePlayableCharacters();
     }
 
@@ -74,19 +62,15 @@ public class Game : BaseMonoBehaviour
         {
             hud.MainCharacterSelect(playerCharacter);
             if (!hasPlayer)
-            {
                 playableCharacters.Insert(0, playerCharacter);
-            }
         }
+
         SetCharacterIdx(0);
         if (curCharacter == null)
-        {
             SetMode(GameMode.Selection);
-        }
+
         foreach (var character in playableCharacters)
-        {
             character.onDeath.AddListener(OnCharacterDied);
-        }
     }
 
 
@@ -99,58 +83,38 @@ public class Game : BaseMonoBehaviour
         foreach(ITimeScalable timeScalable in timeScalables)
         {
             if (timeScalable != null)
-            {
                 timeScalable.SetTimeScale(this.timeScale);
-            }
             else
-            {
                 print("Invalid Object");
-            }
         }
     }
 
-    // Mode
+    // Game Mode
+    public enum GameMode { Selection, Character, Dialogue, Dismiss };
+    private GameMode mode = GameMode.Character;
+    public GameMode Mode { get { return mode; } set { SetMode(value); } }
+    private Dictionary<GameMode, ModeParameters> modeBank;
+    public Dictionary<GameMode, ModeParameters> ModeBank { get { return settings.ModeBank; } }
+    
     public void SetMode(GameMode mode)
     {
-        switch (this.mode)
+        ActivateMode(ModeBank[mode]);
+        if (mode == GameMode.Selection)
         {
-            case GameMode.Character:
-                userInterface.SetHudActive(false); break;
-                //SetControllable(curCharacter, false); break;
-            case GameMode.Selection:
-                userInterface.SetHudActive(false); break;
-                //SetControllable(selector, false); break;
-            case GameMode.Dialogue:
-                userInterface.SetDialogueActive(false); break;
-            case GameMode.Dismiss:
-                userInterface.SetControlScreenActive(false); break;
-        }
-        switch (mode)
-        {
-            case GameMode.Character:
-                userInterface.SetHudActive(true);
-                input.SwitchCurrentActionMap(characterInputMap);
-                TimeScale = 1; break;
-                SetControllable(curCharacter, true); break;
-            case GameMode.Selection:
-                userInterface.SetHudActive(true);
-                input.SwitchCurrentActionMap(selectorInputMap);
-                if (selector != null && curCharacter != null)
-                {
-                    selector.transform.position = curCharacter.body.position;
-                }
-                TimeScale = 0.1f;
-                SetControllable(selector, true); break;
-            case GameMode.Dialogue:
-                userInterface.SetDialogueActive(true);
-                TimeScale = 0f;
-                input.SwitchCurrentActionMap(dialogueInputMap); break;
-            case GameMode.Dismiss:
-                userInterface.SetControlScreenActive(true);
-                TimeScale = 0f;
-                input.SwitchCurrentActionMap(dismissInputMap); break;
+            if (selector != null && curCharacter != null)
+                selector.transform.position = curCharacter.body.position;
         }
         this.mode = mode;
+    }
+
+    public void ActivateMode(ModeParameters mode)
+    {
+        userInterface.SetHudActive(mode.hudActive);
+        userInterface.SetDialogueActive(mode.dialogueActive);
+        userInterface.SetControlScreenActive(mode.controlScreenActive);
+        input.SwitchCurrentActionMap(mode.inputMap);
+        TimeScale = mode.timeScale;
+        if (mode.Controllable != null) SetControllable(mode.Controllable, true);
     }
 
     public void SetControllable(IControllable controllable, bool shouldControl)
@@ -168,7 +132,7 @@ public class Game : BaseMonoBehaviour
             curCharIdx = idx % (playableCharacters.Count);
             Character character = playableCharacters[curCharIdx];
             SetCharacter(character);
-            hud.CharacterSelect(character);//curCharIdx);
+            hud.CharacterSelect(character);
         }
     }
 
@@ -183,12 +147,12 @@ public class Game : BaseMonoBehaviour
         }
     }
     
-    public void SwitchToCompanion(InputValue inputValue, int idx)
+    public void SwitchToCompanion(int idx)
     {
         if (curCharIdx == idx)
-            SetCharacterInput(inputValue, 0);
+            SetCharacterIdx(0);
         else
-            SetCharacterInput(inputValue, idx);
+            SetCharacterIdx(idx);
     }
 
 
@@ -217,142 +181,142 @@ public class Game : BaseMonoBehaviour
 
     // Cheats / Shortcuts
 
-    public void OnRestartLevel(InputValue inputValue)
-    {
-        if (inputValue.isPressed && restartable)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-    }
+    //public void OnRestartLevel(InputValue inputValue)
+    //{
+    //    if (inputValue.isPressed && restartable)
+    //    {
+    //        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //    }
+    //}
 
-    // Movement
+    //// Movement
 
-    public void OnMove(InputValue inputValue)
-    {
-        Vector2 inputVector = inputValue.Get<Vector2>();
-        switch (Mode)
-        {
-            case GameMode.Character:
-                if (CanUseCharacter())
-                {
-                    curCharacter.MoveCharacter(inputVector);
-                } break;
-            case GameMode.Selection:
-                if (CanUseSelector())
-                {
-                    print("Pass move vector to selector");
-                    selector.MoveVector = inputVector;
-                } break;
-        }
-    }
+    //public void OnMove(InputValue inputValue)
+    //{
+    //    Vector2 inputVector = inputValue.Get<Vector2>();
+    //    switch (Mode)
+    //    {
+    //        case GameMode.Character:
+    //            if (CanUseCharacter())
+    //            {
+    //                curCharacter.MoveCharacter(inputVector);
+    //            } break;
+    //        case GameMode.Selection:
+    //            if (CanUseSelector())
+    //            {
+    //                print("Pass move vector to selector");
+    //                selector.MoveVector = inputVector;
+    //            } break;
+    //    }
+    //}
 
-    // Aiming
+    //// Aiming
 
-    public void OnAim(InputValue inputValue) { curCharacter.AimCharacter(inputValue.Get<Vector2>()); }
-    public void OnToggleAiming(InputValue inputValue) { curCharacter.SetAimModeActive(inputValue.isPressed); }
+    //public void OnAim(InputValue inputValue) { curCharacter.AimCharacter(inputValue.Get<Vector2>()); }
+    //public void OnToggleAiming(InputValue inputValue) { curCharacter.SetAimModeActive(inputValue.isPressed); }
 
-    // Castables
+    //// Castables
 
-    public enum CastableIdx { Ability1, Ability2, Weapon1, Weapon2, Agility }
-    public void UseCastable(InputValue inputValue, CastableIdx idx)
-    {
-        if (inputValue.isPressed)
-            curCharacter.ActivateCastable((int)idx);
-    }
-    public void OnUseAgility(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Agility); }
-    public void OnUseWeapon1(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Weapon1); }
-    public void OnUseWeapon2(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Weapon2); }
-    public void OnUseAbility1(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Ability1); }
-    public void OnUseAbility2(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Ability2); }
+    //public enum CastableIdx { Ability1, Ability2, Weapon1, Weapon2, Agility }
+    //public void UseCastable(InputValue inputValue, CastableIdx idx)
+    //{
+    //    if (inputValue.isPressed)
+    //        curCharacter.ActivateCastable((int)idx);
+    //}
+    //public void OnUseAgility(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Agility); }
+    //public void OnUseWeapon1(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Weapon1); }
+    //public void OnUseWeapon2(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Weapon2); }
+    //public void OnUseAbility1(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Ability1); }
+    //public void OnUseAbility2(InputValue inputValue) { UseCastable(inputValue, CastableIdx.Ability2); }
 
-    // Character Switching
+    //// Character Switching
 
-    public void SetCharacterInput(InputValue inputValue, int idx)
-    {
-        if (inputValue.isPressed)
-        {
-            SetCharacterIdx(idx);
-        }
-    }
-    public void OnSwitchCharacterLeft(InputValue inputValue) { SwitchToCompanion(inputValue, 2); }
-    public void OnSwitchCharacterRight(InputValue inputValue) { SwitchToCompanion(inputValue, 1); }
-    public void OnSwitchCharacterCenter(InputValue inputValue) { SetCharacterInput(inputValue, 0); }
-    public void OnCycleCharacterLeft(InputValue inputValue) { SetCharacterInput(inputValue, curCharIdx - 1); }
-    public void OnCycleCharacterRight(InputValue inputValue) { SetCharacterInput(inputValue, curCharIdx + 1); }
+    //public void SetCharacterInput(InputValue inputValue, int idx)
+    //{
+    //    if (inputValue.isPressed)
+    //    {
+    //        SetCharacterIdx(idx);
+    //    }
+    //}
+    //public void OnSwitchCharacterLeft(InputValue inputValue) { SwitchToCompanion(inputValue, 2); }
+    //public void OnSwitchCharacterRight(InputValue inputValue) { SwitchToCompanion(inputValue, 1); }
+    //public void OnSwitchCharacterCenter(InputValue inputValue) { SetCharacterInput(inputValue, 0); }
+    //public void OnCycleCharacterLeft(InputValue inputValue) { SetCharacterInput(inputValue, curCharIdx - 1); }
+    //public void OnCycleCharacterRight(InputValue inputValue) { SetCharacterInput(inputValue, curCharIdx + 1); }
 
-    // Interaction
+    //// Interaction
 
-    public void OnInteract(InputValue inputValue)
-    {
-        if (inputValue.isPressed)
-        {
-            foreach (Interactable interactable in interactables)
-            {
-                interactable.Interact();
-            }
-        }
-    }
+    //public void OnInteract(InputValue inputValue)
+    //{
+    //    if (inputValue.isPressed)
+    //    {
+    //        foreach (Interactable interactable in interactables)
+    //        {
+    //            interactable.Interact();
+    //        }
+    //    }
+    //}
 
-    // Selector
+    //// Selector
 
-    public void OnSelect(InputValue inputValue)
-    {
-        if (inputValue.isPressed)
-        {
-            if (CanUseSelector())
-            {
-                selector.Select();
-            }
-        }
-    }
+    //public void OnSelect(InputValue inputValue)
+    //{
+    //    if (inputValue.isPressed)
+    //    {
+    //        if (CanUseSelector())
+    //        {
+    //            selector.Select();
+    //        }
+    //    }
+    //}
 
-    public void OnDeSelect(InputValue inputValue)
-    {
-        if (inputValue.isPressed)
-        {
-            if (CanUseSelector())
-            {
-                selector.DeSelect();
-            }
-        }
-    }
+    //public void OnDeSelect(InputValue inputValue)
+    //{
+    //    if (inputValue.isPressed)
+    //    {
+    //        if (CanUseSelector())
+    //        {
+    //            selector.DeSelect();
+    //        }
+    //    }
+    //}
 
-    // Skill Wheel
+    //// Skill Wheel
 
-    public void OnToggleSkillWheel(InputValue inputValue)
-    {
-        if (settings.useD20Menu && inputValue.isPressed)
-        {
-            Mode = mode == GameMode.Character ? GameMode.Selection : GameMode.Character;
-            hud.abilityMenu.Toggle();
-        }
-    }
+    //public void OnToggleSkillWheel(InputValue inputValue)
+    //{
+    //    if (settings.useD20Menu && inputValue.isPressed)
+    //    {
+    //        Mode = mode == GameMode.Character ? GameMode.Selection : GameMode.Character;
+    //        hud.abilityMenu.Toggle();
+    //    }
+    //}
 
-    // Control Screen
+    //// Control Screen
 
-    public void OnToggleControls(InputValue inputValue)
-    {
-        if (inputValue.isPressed)
-        {
-            Mode = GameMode.Dismiss;
-        }
-    }
+    //public void OnToggleControls(InputValue inputValue)
+    //{
+    //    if (inputValue.isPressed)
+    //    {
+    //        Mode = GameMode.Dismiss;
+    //    }
+    //}
 
-    public void OnDismiss(InputValue inputValue)
-    {
-        if (inputValue.isPressed)
-        {
-            Mode = GameMode.Character;
-        }
-    }
+    //public void OnDismiss(InputValue inputValue)
+    //{
+    //    if (inputValue.isPressed)
+    //    {
+    //        Mode = GameMode.Character;
+    //    }
+    //}
 
-    // Dialogue
+    //// Dialogue
 
-    public void OnContinue(InputValue inputValue)
-    {
-        if (inputValue.isPressed)
-        {
-            userInterface.Continue();
-        }
-    }
+    //public void OnContinue(InputValue inputValue)
+    //{
+    //    if (inputValue.isPressed)
+    //    {
+    //        userInterface.Continue();
+    //    }
+    //}
 }
