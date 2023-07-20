cce5fa5cdb0cbd5eceae48e9b37c13c78ab346a3
Ian Cullum <ian.p.cullum@gmail.com>
2023-07-19 20:01:24 -0700

    Cleaned up toggling components in Character's SetAlive method using Listeners

--- Assets/Development/Prefabs/Characters/Character.cs
+++ Assets/Development/Prefabs/Characters/Character.cs
@@ -9,6 +9,7 @@ namespace Body
     using Behavior;
     using Body.Behavior.ContextSteering;
     using System.Collections;
+    using System.ComponentModel;
     using UnityEngine.TextCore.Text;
     using static Body.Behavior.ContextSteering.CSIdentity;
 
@@ -18,7 +19,6 @@ namespace Body
     [RequireComponent(typeof(Attack))]
     public class Character : BaseMonoBehaviour, IDamageable, IControllable
     {
-        // Movement and Positioning
         [Header("Movement and Positioning")]
         public Transform body;
         public Transform pivot;
@@ -26,40 +26,35 @@ namespace Body
         [HideInInspector] public Movement movement;
         [HideInInspector] public float baseOffset;
 
-        // State
         [Header("State")]
         public bool controllable = true;
         public bool aimActive = false;
         public bool alive = false;
+        [Space]
+        public UnityEvent<bool> onControl;
 
-        // Appearance
         [Header("Appearance")]
         public ArtRenderer artRenderer;
         public CinemachineVirtualCamera virtualCamera;
         public CharacterUIElements characterUIElements;
 
-        // Collision
         [Header("Collision")]
         public Collider aliveCollider;
         public Collider deadCollider;
 
-        // Interaction
         [Header("Interaction")]
         public Interactor interactor;
         [HideInInspector] public Talker talker;
 
-        // Behaviour
         [Header("Behaviour")]
         [HideInInspector] public Brain brain;
         public CSController Controller { get => brain.controller; }
 
-        // Castables
         [Header("Casting")]
         public Loadout loadout;
         public Transform weaponOffset;
         [HideInInspector] public Attack attacker;
 
-        // Identity
         [Header("Identity")]
         public Identity identity = Identity.Neutral;
         public Identity Identity
@@ -72,17 +67,17 @@ namespace Body
             }
         }
 
-        // Statuses
         [Header("Status Effects")]
         public List<Status> statuses;
 
-        // Health
         [Header("Health and Damage")]
         public Health healthBar;
         public bool alwaysHideHealth = false;
         public float hideHealthWaitTime = 15f;
         public Modified<int> maxHealth = new(20);
         public Modified<int> currentHealth = new(20);
+        [Space]
+        public UnityEvent onDmg;
         public int MaxHealth
         {
             get { return maxHealth.Value; }
@@ -94,15 +89,12 @@ namespace Body
             set { SetCurrentHealth(value); }
         }
 
-        // Respawn
-        [Header("Respawn")]
+        [Header("Death and Respawning")]
         public Transform spawn;
-
-        // Events
+        [Space]
         public UnityEvent<Character> onDeath;
-        public UnityEvent onDmg;
         public UnityEvent onRespawn;
-        public UnityEvent<bool> onControl;
+        public UnityEvent<bool> onAlive;
 
 
         // Initialization
@@ -118,12 +110,11 @@ namespace Body
             movement = GetComponent<Movement>();
             talker = GetComponent<Talker>();
             attacker = GetComponent<Attack>();
-            
+
             // Connections
-            onControl.AddListener(moveReticle.gameObject.SetActive);
-            onControl.AddListener(virtualCamera.gameObject.SetActive);
-            onControl.AddListener(interactor.gameObject.SetActive);
-            
+            ConnectAlive();
+            ConnectControl();
+
             // Initialization
             InitializeCastables();
             MaxHealth = MaxHealth;
@@ -174,8 +165,7 @@ namespace Body
 
         public void Respawn()
         {
-            body.position = spawn.position;
-            body.rotation = spawn.rotation;
+            body.SetPositionAndRotation(spawn.position, spawn.rotation);
             body.localScale = spawn.localScale;
             Refresh();
         }
@@ -209,36 +199,34 @@ namespace Body
             onControl.Invoke(_controllable);
         }
 
-        public void SubscribeToControl(Behaviour behaviour)
+        public void ConnectControl()
         {
-            if (behaviour != null) onControl.AddListener((bool controlled) => { behaviour.enabled = controlled; });
+            if (moveReticle != null) onControl.AddListener(moveReticle.gameObject.SetActive);
+            if (virtualCamera != null) onControl.AddListener(virtualCamera.gameObject.SetActive);
+            if (interactor != null) onControl.AddListener(DoEnable(interactor));
         }
-
-        public void SubscribeToControl(Component component)
-        {
-            if (component != null) onControl.AddListener(component.gameObject.SetActive);
-        }
+        private UnityAction<bool> DoEnable(Behaviour behaviour, bool disable=false) { return (bool enable) => { behaviour.enabled = enable && !disable; }; }
 
 
         // Health
+
+        private void ConnectAlive()
+        {
+            onAlive.AddListener((bool alive) => { brain.Alive = alive; });
+            onAlive.AddListener(DoEnable(movement));
+            onAlive.AddListener(DoEnable(attacker));
+
+            if (artRenderer != null) onAlive.AddListener((bool alive) => { artRenderer.Dead = !alive; });
+            if (aliveCollider != null) onAlive.AddListener((bool alive) => { aliveCollider.enabled = alive; });
+            if (deadCollider != null) onAlive.AddListener((bool alive) => { deadCollider.enabled = alive; });
+        }
 
         public void SetAlive(bool alive)
         {
             movement.SetMoveVector(new());
             bool died = !alive && this.alive;
             this.alive = alive;
-            if (movement != null)
-                movement.enabled = alive;
-            if (attacker != null) 
-                attacker.enabled = alive;
-            if (brain != null)
-                brain.Alive = alive;
-            if (artRenderer != null)
-                artRenderer.Dead = !alive;
-            if (aliveCollider != null)
-                aliveCollider.enabled = alive;
-            if (deadCollider != null)
-                deadCollider.enabled = !alive;
+            onAlive.Invoke(alive);
             if (died)
                 onDeath.Invoke(this);
         }
