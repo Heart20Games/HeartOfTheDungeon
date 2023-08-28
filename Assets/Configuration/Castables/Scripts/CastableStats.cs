7bd50af3e0ce365eb98fa88494dd8a076d6d6fa5
Ian Cullum <cullumi@wwu.edu>
2023-08-27 21:13:24 -0700

    Fleshed out ranged weapon castable generation
    TODO: Fix a couple issues that make the final product not work correctly.

--- Assets/Configuration/Castables/Scripts/CastableStats.cs
+++ Assets/Configuration/Castables/Scripts/CastableStats.cs
@@ -1,3 +1,4 @@
+using MyBox;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,13 +8,29 @@ using UnityEngine;
 public class CastableStats
 {
     public string usabilityTag = "None";
-    public int baseDamage = 1;
-    public float baseCooldown = 1;
-    public float baseKnockback = 1;
-    public float baseRange = 1;
-    public int baseCastStatusPower;
-    public List<StatusEffect> castStatuses;
-    public int baseHitStatusPower;
-    public List<StatusEffect> hitStatuses;
+
+    [Header("Damage")]
+    public bool dealDamage = false;
+    [ConditionalField("dealDamage", false, true)]
+    [Range(0, 10)] public int baseDamage = 1;
+
+    [Header("Cooldown")]
+    public bool useCooldown = false;
+    [ConditionalField("useCooldown", false, true)]
+    [Range(0, 10)] public float baseCooldown = 1;
+
+    [Header("Knockback")]
+    [Range(0, 10)] public float baseKnockback = 1;
+
+    [Header("Range")]
+    [Range(0, 10)] public float baseRange = 1;
+
+    [Header("Statuses")]
+    [Range(0, 3)] public int baseCastStatusPower;
+    public List<Status> castStatuses;
+    [Range(0, 3)] public int baseHitStatusPower;
+    public List<Status> hitStatuses;
+
+    [Header("Bonuses")]
     public List<StatBonus> statBonuses;
 }
