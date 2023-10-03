8ebe826ee1c8f92fa44bf6e0d0ec3aa498b82b0f
Ian Cullum <cullumi@wwu.edu>
2023-10-02 17:44:27 -0700

    Added ButtonMethodAttribute from MyBox

--- Assets/Development/Prefabs/Direction/SceneTimeline/SceneTimeline.cs
+++ Assets/Development/Prefabs/Direction/SceneTimeline/SceneTimeline.cs
@@ -25,6 +25,7 @@ public class SceneTimeline : BaseMonoBehaviour
         UpdateCutsceneBank();
     }
 
+    [ButtonMethod]
     public void UpdateCutsceneBank()
     {
         cutsceneBank.Clear();
