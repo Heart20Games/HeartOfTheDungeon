828f3c3b711d2cf8d8ad47f051e7861ef16318f8
Ian Cullum <cullumi@wwu.edu>
2024-01-26 14:05:39 -0800

    Added DigitLibrary scriptable object (supports ordered digits and +/-)

---
+++ Assets/Configuration/ScriptableObjects/Digits/DigitLibrary.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DigitLibrary", menuName = "DigitLibrary", order = 1)]
+public class Digits : BaseScriptableObject
+{
+    public Sprite[] numbers;
+    public Sprite plus;
+    public Sprite minus;
+
+    public Sprite GetNumber(int number)
+    {
+        return numbers[number];
+    }
+
+    public Sprite GetSign(int sign)
+    {
+        return sign < 0 ? minus : sign > 0 ? plus : null;
+    }
+}
