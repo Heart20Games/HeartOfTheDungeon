a26eb62cdec0abfee984c821e0ca7098de95e12e
Ian Cullum <cullumi@wwu.edu>
2024-02-16 19:03:23 -0800

    Added Range Attributes to Death Post Processing's serialized fields.
    Also: Saturation Rate is now assumed to be positive. Has the exact same effect as before, though.

--- Assets/ArtAndStuff/Art/VFX/Post Processing Effects/DeathPostProcessing.cs
+++ Assets/ArtAndStuff/Art/VFX/Post Processing Effects/DeathPostProcessing.cs
@@ -11,14 +11,14 @@ public class DeathPostProcessing : MonoBehaviour
     private VolumeProfile volumeProfile;
     private Vignette vignette;
     private ColorAdjustments colorAdjustments;
-    public float startingSaturation; // 100 is fully saturated, -100 is full black and white.
-    [SerializeField] private float targetSaturation;
-    [SerializeField] private float targetVignetteIntensity; // 0 is no vignette, 1 max vignette.
+    [Range(-100, 100)] public float startingSaturation = 24.3f; // 100 is fully saturated, -100 is full black and white.
+    [Range(-100, 100)][SerializeField] private float targetSaturation = -100;
+    [Range(0, 1)][SerializeField] private float targetVignetteIntensity = 0; // 0 is no vignette, 1 max vignette.
     private float currentSaturation;
     private float currentVignetteIntensity;
-    [SerializeField] private float saturationRate; // How quickly it transitions from starting to target (amount per refresh rate).
-    [SerializeField] private float vignetteIntensityRate; // How quickly it transitions from 0 to target.
-    [SerializeField] private float refreshRate; // Time in seconds till refreshing.
+    [Range(0.01f, 20)][SerializeField] private float saturationRate = 6f; // How quickly it transitions from starting to target (amount per refresh rate).
+    [Range(0.001f, 1f)][SerializeField] private float vignetteIntensityRate = 0.02f; // How quickly it transitions from 0 to target.
+    [Range(0.001f, 1f)][SerializeField] private float refreshRate = 0.02f; // Time in seconds till refreshing.
     [SerializeField] private bool testDeath;
     [SerializeField] private bool testDeathDeactivate;
     
@@ -72,7 +72,7 @@ public class DeathPostProcessing : MonoBehaviour
         {
             if(currentSaturation < startingSaturation)
             {
-                colorAdjustments.saturation.Override(currentSaturation - saturationRate);
+                colorAdjustments.saturation.Override(currentSaturation + saturationRate);
                 currentSaturation =  colorAdjustments.saturation.value;
             }
             if(currentVignetteIntensity > 0f)
@@ -95,7 +95,7 @@ public class DeathPostProcessing : MonoBehaviour
         {
             if(currentSaturation > targetSaturation)
             {
-                colorAdjustments.saturation.Override(currentSaturation + saturationRate);
+                colorAdjustments.saturation.Override(currentSaturation - saturationRate);
                 currentSaturation =  colorAdjustments.saturation.value;
             }
             if(currentVignetteIntensity < targetVignetteIntensity)
