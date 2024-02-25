940bb20e47fb95c821865a438a6887fc9988a2ed
Ian Cullum <cullumi@wwu.edu>
2024-02-25 04:13:33 -0800

    Character emotions now trigger Unity events that can be subscribed to.

--- Assets/Development/Prefabs/Game Management/Identifiable.cs
+++ Assets/Development/Prefabs/Game Management/Identifiable.cs
@@ -4,6 +4,7 @@ using Palmmedia.ReportGenerator.Core;
 using System;
 using UIPips;
 using UnityEngine;
+using UnityEngine.Events;
 using static Body.Behavior.ContextSteering.CSIdentity;
 
 public interface IIdentifiable
@@ -14,20 +15,30 @@ public interface IIdentifiable
     public string Emotion { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public ModField<int> Health { get; }
+    public MaxModField<int> Health { get; }
 
+    public void ConnectPortrait(UnityAction<PortraitImage> action, bool initialize = false);
+    public void DisconnectPortrait(UnityAction<PortraitImage> action);
+    public void ConnectImage(UnityAction<Sprite> action, bool initialize = false);
+    public void DisconnectImage(UnityAction<Sprite> action);
     public void ConnectPips(PipGenerator generator, bool initialize = false);
     public void DisconnectPips(PipGenerator generator);
 }
 
 public abstract class AIdentifiable : BaseMonoBehaviour, IIdentifiable
 {
+    // Fields
     [Foldout("Identity", true)]
     [Header("Identity")]
     public Portraits portraits = null;
     public Identity identity = Identity.Neutral;
-    [Foldout("Identity")] public string emotion = "neutral";
-    
+    [Foldout("Identity")] public ModField<string> emotion = new("Emotion", "neutral");
+
+    // Events
+    public UnityEvent<PortraitImage> onPortrait;
+    public UnityEvent<Sprite> onImage;
+
+    // Properties
     public abstract Identity Identity { get; set; }
     public virtual PortraitImage Portrait
     {
@@ -39,18 +50,54 @@ public abstract class AIdentifiable : BaseMonoBehaviour, IIdentifiable
         get => portraits.GetImage(Name, Emotion);
         set => NULL();
     }
-    public virtual string Emotion { get => emotion; set => emotion = value; }
+    public virtual string Emotion { get => emotion.current.Value; set => emotion.current.Value = value; }
     public virtual string Name { get; set; }
     public virtual string Description { get => ""; set => NULL(); }
 
     // Mod Fields
-    public abstract ModField<int> Health { get; }
-    public abstract ModField<int> Armor { get; }
+    public abstract MaxModField<int> Health { get; }
+    public abstract MaxModField<int> Armor { get; }
 
     protected void NULL() { /* Do Nothing */ }
 
+    // Initialization
+    private void Awake()
+    {
+        emotion.Subscribe(UpdatePortraits);
+    }
+
+    // Portrait Connections
+    private void UpdatePortraits(string _emotion)
+    {
+        onPortrait.Invoke(Portrait);
+        onImage.Invoke(Image);
+    }
+
+    public void ConnectPortrait(UnityAction<PortraitImage> action, bool initialize = false)
+    {
+        onPortrait.AddListener(action);
+        if (initialize)
+            action.Invoke(Portrait);
+    }
+    public void DisconnectPortrait(UnityAction<PortraitImage> action)
+    {
+        onPortrait.RemoveListener(action);
+    }
+
+    public void ConnectImage(UnityAction<Sprite> action, bool initialize = false)
+    {
+        onImage.AddListener(action);
+        if (initialize)
+            action.Invoke(Image);
+    }
+    public void DisconnectImage(UnityAction<Sprite> action)
+    {
+        onImage.RemoveListener(action);
+    }
+
+
     // Pip Connections
-    public void ConnectPipType(ModField<int> modField, Modded<int>.Listen current, Modded<int>.Listen total, bool initialize = false)
+    public void ConnectPipType(MaxModField<int> modField, Modded<int>.Listen current, Modded<int>.Listen total, bool initialize = false)
     {
         Health.Subscribe(current, total);
         if (initialize)
@@ -68,7 +115,7 @@ public abstract class AIdentifiable : BaseMonoBehaviour, IIdentifiable
         }
     }
 
-    public void DisconnectPipType(ModField<int> modField, Modded<int>.Listen current, Modded<int>.Listen total)
+    public void DisconnectPipType(MaxModField<int> modField, Modded<int>.Listen current, Modded<int>.Listen total)
     {
         Health.UnSubscribe(current, total);
     }
@@ -86,6 +133,6 @@ public class Identifiable : AIdentifiable
 {
     public override Identity Identity { get => identity; set => identity = value; }
     public override string Name { get => name; set => name = value; }
-    public override ModField<int> Health { get => null; }
-    public override ModField<int> Armor { get => null; }
+    public override MaxModField<int> Health { get => null; }
+    public override MaxModField<int> Armor { get => null; }
 }
