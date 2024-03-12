5ea4c7824d8973fd1629fa15c43f769345d11f26
Ian Cullum <cullumi@wwu.edu>
2024-03-12 00:06:00 -0700

    RestartLife spawns that party at a checkpoint if possible now.

--- Assets/Development/Prefabs/Game Management/Checkpoint.cs
+++ Assets/Development/Prefabs/Game Management/Checkpoint.cs
@@ -2,6 +2,7 @@ using MyBox;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Checkpoint : BaseMonoBehaviour
 {
@@ -12,6 +13,11 @@ public class Checkpoint : BaseMonoBehaviour
 
     [Header("Party")]
     public Party targetParty;
+
+    [Foldout("Events", true)]
+    public UnityEvent onInteract;
+    [Foldout("Events")]
+    public UnityEvent onSpawn;
 
     public string Name
     {
@@ -25,6 +31,12 @@ public class Checkpoint : BaseMonoBehaviour
     }
 
     [ButtonMethod]
+    public void Interact()
+    {
+        onInteract.Invoke();
+    }
+
+    [ButtonMethod]
     public void SpawnAtCheckpoint()
     {
         SpawnAtCheckpoint(targetParty);
@@ -34,5 +46,6 @@ public class Checkpoint : BaseMonoBehaviour
     {
         party.transform.position = transform.position;
         party.Respawn();
+        targetParty = party;
     }
 }
