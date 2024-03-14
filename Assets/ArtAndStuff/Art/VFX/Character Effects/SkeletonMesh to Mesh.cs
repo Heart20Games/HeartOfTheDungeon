b006a779a7b51cf0301dffb3153971e5bf3d01ba
JJArakus <Jeremiah@arakusband.com>
2024-03-14 01:12:52 -0700

    Created AOE Indicator Decal and VFX

--- Assets/ArtAndStuff/Art/VFX/Character Effects/SkeletonMesh to Mesh.cs
+++ Assets/ArtAndStuff/Art/VFX/Character Effects/SkeletonMesh to Mesh.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -10,6 +11,12 @@ public class SkeletonMeshtoMesh : MonoBehaviour
     public MeshFilter skeletonMesh;
     public VisualEffect vfxGraph;
     public float refreshRate;
+    public Transform meshTransform;
+    public UnityEngine.Vector3 scale;
+    public UnityEngine.Vector3 position;
+    public UnityEngine.Vector3 rotation;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +30,12 @@ public class SkeletonMeshtoMesh : MonoBehaviour
             Mesh m = new Mesh();
             m = skeletonMesh.mesh;
             vfxGraph.SetMesh("Mesh", m);
+            scale = meshTransform.localScale;
+            position = meshTransform.localPosition;
+            rotation = meshTransform.localEulerAngles;
+            vfxGraph.SetVector3("Scale", scale);
+            vfxGraph.SetVector3("Position", position);
+            vfxGraph.SetVector3("Rotation", rotation);
 
             yield return new WaitForSeconds (refreshRate);
         }
