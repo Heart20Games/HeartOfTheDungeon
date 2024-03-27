0e762eca1679b7b01fd18adacf14025da4312653
Ian Cullum <cullumi@wwu.edu>
2024-03-26 23:23:37 -0700

    Expanded session -> story pipeline for parties and yarn nodes.
    Also:
    + Implemented scene progress as a dictionary, defeated parties and reached yarn nodes as HashSets for the Session and Story scriptable objects.
    + Implemented transition from Dictionary and HashSets to Lists when saving to Session and Story Data.
    + Wrote code to reset dialogue triggers and improved how parties are selected for spawning.

--- Assets/Development/Prefabs/Game Management/Progress/ProgressManager.cs
+++ Assets/Development/Prefabs/Game Management/Progress/ProgressManager.cs
@@ -1,3 +1,4 @@
+using HotD;
 using MyBox;
 using System.Collections;
 using System.Collections.Generic;
@@ -16,14 +17,17 @@ public class ProgressManager : BaseMonoBehaviour
     [Header("Objects")]
     public List<Checkpoint> checkpoints;
     public List<Party> parties;
+    public List<DialogueTrigger> dialogueTriggers;
 
     // Settings
     [Header("Settings")]
-    public bool alwaysSpawnAll = false;
+    [Tooltip("Respawn all parties when respawning?")] public bool alwaysSpawnAll = false;
+    [Tooltip("Reset all dialogue triggers when respawning?")] public bool alwaysResetDialogues = false;
     public bool debug = false;
 
     private void Awake()
     {
+        // Checkpoints
         if (checkpoints == null || checkpoints.Count == 0)
         {
             checkpoints = new List<Checkpoint>(FindObjectsOfType<Checkpoint>());
@@ -31,12 +35,98 @@ public class ProgressManager : BaseMonoBehaviour
             {
                 if (checkpoint.session == null || (session != null && checkpoint.session != session))
                     checkpoint.session = session;
+                checkpoint.onActivate.AddListener(SaveSessionData);
             }
         }
+        // Parties
         if (parties == null || parties.Count == 0)
         {
             parties = new List<Party>(FindObjectsOfType<Party>());
+            foreach (var party in parties)
+            {
+                party.onAllDead.AddListener(() => { AddPartyDefeated(party.Name); });
+            }
         }
+        // Dialogue Triggers
+        if (dialogueTriggers == null || dialogueTriggers.Count == 0)
+        {
+            dialogueTriggers = new List<DialogueTrigger>(FindObjectsOfType<DialogueTrigger>());
+            foreach (var trigger in dialogueTriggers)
+            {
+                trigger.onDoneTalking.AddListener(() => { AddYarnNodeReached(trigger.targetNode); });
+            }
+        }
+    }
+
+    // Respawn Everything
+
+    public void RespawnToLastCheckpoint()
+    {
+        ActivateDialogues();
+        SpawnParties();
+        SpawnAtCheckpoint(Party.mainParty);
+    }
+
+    // Session Date Management
+
+    public void SaveSessionData()
+    {
+        Assert.IsNotNull(session);
+        Assert.IsNotNull(story);
+
+        // Scene Progress
+        story.TryGetProgress(session.name, out _, true);
+        SceneProgress progress = story.sceneProgress[session.name];
+        progress.checkpointsReached.Add(session.checkpoint);
+        progress.partiesDefeated.UnionWith(session.unsavedPartiesDefeated);
+        
+        // Yarn Node Progress
+        story.yarnNodesReached.UnionWith(session.unsavedYarnNodesReached);
+        session.unsavedYarnNodesReached.Clear();
+    }
+
+    public void AddPartyDefeated(string partyName)
+    {
+        session.unsavedPartiesDefeated.Add(partyName);
+    }
+
+    public void AddYarnNodeReached(string yarnNode)
+    {
+        session.unsavedYarnNodesReached.Add(yarnNode);
+    }
+
+    // Reset Dialogues
+
+    public void ActivateDialogues()
+    {
+        if (alwaysResetDialogues)
+            ActivateAllDialogues();
+        else
+            ActivateSomeDialogues();
+    }
+
+    public void ActivateAllDialogues()
+    {
+        foreach (DialogueTrigger trigger in dialogueTriggers)
+        {
+            trigger.SetActive(true);
+        }
+    }
+
+    public void ActivateSomeDialogues()
+    {
+        foreach (DialogueTrigger trigger in dialogueTriggers)
+        {
+            trigger.SetActive(IsYarnNodeReached(trigger.targetNode));
+        }
+    }
+
+    public bool IsYarnNodeReached(string node)
+    {
+        return (
+            session.unsavedYarnNodesReached.Contains(node) ||
+            !story.yarnNodesReached.Contains(node)
+        );
     }
 
     // Spawn Parties (All vs NonDefeated)
@@ -49,16 +139,27 @@ public class ProgressManager : BaseMonoBehaviour
             SpawnNonDefeated();
     }
 
+    private void SpawnAll()
+    {
+        Print("Spawn All Enemies.");
+        foreach (Party party in parties)
+        {
+            if (party != Party.mainParty)
+            {
+                Print($"Spawning {party.Name}.", debug);
+                party.Respawn();
+            }
+        }
+    }
+
     private void SpawnNonDefeated()
     {
         Print("Spawn Only Non_Defeated Enemies.", debug);
-        Scene scene = SceneManager.GetActiveScene();
-        SceneProgress progress = story.GetProgress(scene.name);
         foreach (var party in parties)
         {
             if (party != Party.mainParty)
             {
-                if (progress.groupsDefeated.Contains(party.Name))
+                if (IsPartyDefeated(party.Name))
                 {
                     Print($"Despawning {party.Name}", debug);
                     foreach (var member in party.members)
@@ -76,18 +177,14 @@ public class ProgressManager : BaseMonoBehaviour
         }
     }
 
-    private void SpawnAll()
+    public bool IsPartyDefeated(string node)
     {
-        Print("Spawn All Enemies.");
-        foreach(Party party in parties)
-        {
-            if (party != Party.mainParty)
-            {
-                Print($"Spawning {party.Name}.", debug);
-                party.Respawn();
-            }
-        }
+        return (
+            session.unsavedPartiesDefeated.Contains(node) ||
+            (story.GetProgress(session.scene).partiesDefeated?.Contains(node) == false)
+        );
     }
+
 
     /// <summary>Spawns the given party at the checkpoint found on the current session.</summary>
     /// <returns>Returns true if a valid checkpoint was found.</returns>
