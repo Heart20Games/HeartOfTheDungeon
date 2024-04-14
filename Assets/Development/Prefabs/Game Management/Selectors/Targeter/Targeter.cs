4fa6a29838e5f076f4aa08b96479f3c1962b65c2
Ian Cullum <cullumi@wwu.edu>
2024-04-14 04:47:54 -0700

    Targeter and TargetFinder know to update when IsSelectable changes on ASelectables now.

--- Assets/Development/Prefabs/Game Management/Selectors/Targeter/Targeter.cs
+++ Assets/Development/Prefabs/Game Management/Selectors/Targeter/Targeter.cs
@@ -149,16 +149,33 @@ namespace Selection
             } 
         }
 
+        public void SwitchTargets(int newIdx)
+        {
+            // Set selected to the next nearest selectable in the given direction.
+            DeSelect();
+            finder.TargetIdx = newIdx % finder.selectables.Count;
+            Select();
+            onTargetSet.Invoke(selected);
+        }
         public void SwitchTargets(bool left)
         {
             Print($"Switch targets {(left ? "left" : "right")}.", debug);
-            if (finder.selectables.Count > 1)
+            int newIdx = finder.TargetIdx + (left ? -1 : 1);
+            if (newIdx > 0 && finder.selectables.Count > newIdx)
             {
-                // Set selected to the next nearest selectable in the given direction.
-                DeSelect();
-                finder.TargetIdx += (left ? -1 : 1);
-                Select();
-                onTargetSet.Invoke(selected);
+                SwitchTargets(newIdx);
+            }
+        }
+
+        public void TargetLost(bool stillThere=true)
+        {
+            Print($"Target lost? {(!stillThere ? "Yes" : "No")}", debug);
+            if (!stillThere)
+            {
+                if (finder.selectables.Count > 1)
+                    SwitchTargets(finder.TargetIdx + 1);
+                else
+                    DeSelect();
             }
         }
 
@@ -175,11 +192,18 @@ namespace Selection
         {
             base.Select();
             if (selected != null)
+            {
                 targetGroup.m_Targets[1].target = selected.transform;
+                selected.onIsSelectable.AddListener(TargetLost);
+            }
         }
 
         public override void DeSelect()
         {
+            if (selected != null)
+            {
+                selected.onIsSelectable.RemoveListener(TargetLost);
+            }
             base.DeSelect();
             targetGroup.m_Targets[1].target = null;
         }
