3d1be9b736b595085742acaf518321db01908aae
Ian Cullum <cullumi@wwu.edu>
2024-04-15 22:01:19 -0700

    Fixed negative size error on Magic Bolt Level 3. Refactored Level 3 Bolt Scaling using Coroutines.

--- Assets/ArtAndStuff/Art/VFX/Magic Bolt V2/Level 3 Bolt Scaling.cs
+++ Assets/ArtAndStuff/Art/VFX/Magic Bolt V2/Level 3 Bolt Scaling.cs
@@ -10,7 +10,9 @@ public class Level3BoltScaling : MonoBehaviour
     public float scaleSpeed;
     public float castDuration;
     private bool playing = false;
+    private bool casting = false;
     [SerializeField] private float currentScale;
+    private Coroutine windDownCoroutine;
 
     // Start is called before the first frame update
     void Start()
@@ -18,40 +20,66 @@ public class Level3BoltScaling : MonoBehaviour
         currentScale = 0f;
     }
 
+    public void ChargeUp()
+    {
+        bolt.SetFloat("Duration", castDuration);
+        bolt.SetBool("Effect End", false);
+        bolt.Play();
+        playing = true;
+        currentScale = 0;
+        InterruptWindDown();
+    }
+
+    public void InterruptWindDown()
+    {
+        if (windDownCoroutine != null)
+        {
+            StopCoroutine(windDownCoroutine);
+            windDownCoroutine = null;
+        }
+    }
+
+    public void WindDown()
+    {
+        InterruptWindDown();
+        windDownCoroutine = StartCoroutine(DoWindDown(castDuration));
+    }
+
+    private IEnumerator DoWindDown(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        bolt.Stop();
+        currentScale = 0f;
+        enabled = false;
+        playing = false;
+        casting = false;
+        bolt.SetBool("Effect End", true);
+        windDownCoroutine = null;
+    }
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (isActiveAndEnabled)
         {
             if (!playing)
             {
-                bolt.SetFloat("Duration", castDuration);
-                bolt.SetBool("Effect End", false);
-                bolt.Play();
-                playing = true;
-            } 
-            if (currentScale < 50f)
-            {
-                currentScale += Time.deltaTime * scaleSpeed;
-                currentScale = Mathf.Clamp(currentScale, 0f, 50f);
+                ChargeUp();
             }
-            else if (currentScale >= 50 && (currentScale - 50) < castDuration)
-            {
-                currentScale += Time.deltaTime;
-            }
-            else if (currentScale >= 50 && (currentScale - 50) >= castDuration)
-            {
-                bolt.Stop();
-                currentScale = 0f;
-                enabled = false;
-                playing = false;
-                bolt.SetBool("Effect End", true);
-            }
-            else if (currentScale != 0f) currentScale = 0;
-        }
-        else if (currentScale != 0f) currentScale = 0;
 
-        bolt.SetFloat("Scale", currentScale);
-        meshCollider.transform.localScale = new Vector3(meshCollider.transform.localScale.x, meshCollider.transform.localScale.y, (currentScale * 100));
+            if (!casting)
+            {
+                currentScale += Time.fixedDeltaTime * scaleSpeed;
+                float clampedScale = Mathf.Min(currentScale, 50f);
+                if (currentScale != clampedScale)
+                {
+                    currentScale = clampedScale;
+                    casting = true;
+                    WindDown();
+                }
+                bolt.SetFloat("Scale", currentScale);
+                meshCollider.transform.localScale = new Vector3(meshCollider.transform.localScale.x, meshCollider.transform.localScale.y, (currentScale * 100));
+            }
+        }
     }
 }
