cb9bf71c2fbd996c70d841f00c8b626600a82222
Matthew <Music.matthew.wood@gmail.com>
2024-04-26 10:59:46 -0400

    Made change to EffectSpawner script.

--- Assets/Development/Prefabs/Castables/EffectSpawner.cs
+++ Assets/Development/Prefabs/Castables/EffectSpawner.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using FMODUnity;
 
 public class EffectSpawner : Positionable
 {
+    [SerializeField] private StudioEventEmitter deathPoof;
     [Serializable]
     public struct Effect
     {
@@ -43,6 +45,7 @@ public class EffectSpawner : Positionable
     {
         foreach (Effect effect in effects)
         {
+            deathPoof.Play();
             Transform tform = Instantiate(effect.prefab, source);
             tform.position = target.position + effect.prefab.transform.position;
             StartCoroutine(CleanupInstance(new Instance(effect, tform)));
