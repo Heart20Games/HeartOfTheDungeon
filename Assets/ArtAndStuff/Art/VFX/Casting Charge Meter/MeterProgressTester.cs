9ee4cf333e01210aa375c49b6ae500fba5e74b8c
JJArakus <Jeremiah@arakusband.com>
2024-08-12 20:33:32 -0700

    Improvements to Casting Charge Meter and updated UI camera to add post processing

--- Assets/ArtAndStuff/Art/VFX/Casting Charge Meter/MeterProgressTester.cs
+++ Assets/ArtAndStuff/Art/VFX/Casting Charge Meter/MeterProgressTester.cs
@@ -9,6 +9,7 @@ public class MeterProgressTester : MonoBehaviour
     public VisualEffect castingMeterFill;
     public VisualEffect castingMeterBackground;
     [Range(0, 1)]public float meterProgress;
+    public bool levelUp;
     
 
     // Update is called once per frame
@@ -16,5 +17,10 @@ public class MeterProgressTester : MonoBehaviour
     {
         castingMeterFill.SetFloat("Meter Progress", meterProgress);
         castingMeterBackground.SetFloat("Meter Progress", meterProgress);
+        if (levelUp)
+        {
+            castingMeterFill.SendEvent("Level Up 2");
+            levelUp = false;
+        }
     }
 }
