bafd94ca22f276145a948dd031a493a1c669145f
Enkidu1988 <46576304+Enkidu1988@users.noreply.github.com>
2024-09-01 12:26:44 -0700

    Articy Draft
    Articy draft implementation

---
+++ Assets/MyArticyPluginComponent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Articy.Unity;
+
+public class MyArticyPluginComponent : MonoBehaviour
+{
+    public ArticyRef myFirstArticyModel;
+
+    void Start()
+    {
+        var techName = myFirstArticyModel.GetObject().TechnicalName;
+        Debug.Log(techName);
+    }
+}
