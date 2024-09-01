1ae752384f95e03fcaf71bc73ab2727538c1685c
Enkidu1988 <46576304+Enkidu1988@users.noreply.github.com>
2024-09-01 13:51:32 -0700

    Added Artify Draft

--- Assets/MyArticyPluginComponent.cs
+++ Assets/MyArticyPluginComponent.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public class MyArticyPluginComponent : MonoBehaviour
+using UnityEngine;
 using Articy.Unity;
 
 public class MyArticyPluginComponent : MonoBehaviour
