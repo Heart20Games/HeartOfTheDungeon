9db671e36787504288aef6b2516dbc1cdf0832d7
JJArakus <Jeremiah@arakusband.com>
2024-09-13 16:45:32 -0700

    Implemented Magic Shield Charge Meter Controller Script

--- Assets/ArtAndStuff/Art/VFX/Magic Shield Charge Meter/MagicShieldChargeMeterController.cs
+++ Assets/ArtAndStuff/Art/VFX/Magic Shield Charge Meter/MagicShieldChargeMeterController.cs
@@ -11,6 +11,13 @@ public class MagicShieldChargeMeterController : MonoBehaviour
     public bool shieldBreak = false;
     private bool shieldBroken = false;
     public bool shieldRestore = false;
+    //0 to 1f VFX parameter that determines the alpha level of the cracks
+    private float crackAlpha;
+    //0.1f to 1f VFX parameter that determines the alpha level of the top layer cracks
+    private float topCrackFade;
+    //-1f to 0f VFX parameter that increases the intensity of the cracks as it goes lower
+    private float breakModifier;
+    [SerializeField] private float crackFadeSpeed = 2;
 
 
     // Update is called once per frame
@@ -19,29 +26,58 @@ public class MagicShieldChargeMeterController : MonoBehaviour
         if (meterVFX != null)
         {
             meterVFX.SetFloat("Mask Position", meterPosition);
-            if (shieldBreak)
+            if (shieldBreak && !shieldBroken)
             {
-                ShieldBreak();
+                StartCoroutine(ShieldBreak());
             }
             if (shieldRestore)
             {
-                ShieldRestore();
+                StartCoroutine(ShieldRestore());
             }
         }
     }
 
-    void ShieldBreak()
+    private IEnumerator ShieldBreak()
     {
+        shieldBroken = true;
+        topCrackFade = 1f;
+        crackAlpha = 0f;
+        breakModifier = -1f;
         meterVFX.SetBool("ShieldBreak", true);
         meterVFX.SendEvent("ShieldBreak");
-        shieldBroken = true;
+        while (crackAlpha < 1 && !shieldRestore)
+        {
+            crackAlpha += .05f;
+            topCrackFade -= .05f;
+            breakModifier += .05f;
+            crackAlpha = Mathf.Clamp(crackAlpha, 0f, 1f);
+            topCrackFade = Mathf.Clamp(topCrackFade, 0.01f, 1f);
+            breakModifier = Mathf.Clamp(breakModifier,-1f, 0f);
+            Debug.Log(topCrackFade);
+            meterVFX.SetFloat("CrackAlphaClipping", crackAlpha);
+            meterVFX.SetFloat("TopLayerCrackErosion", topCrackFade);
+            meterVFX.SetFloat("BreakModifier", breakModifier);
+            yield return new WaitForSeconds(crackFadeSpeed/75f);
+        }
+        breakModifier = 0f;
+        
     }
 
-    void ShieldRestore()
-    {
-        meterVFX.SetBool("ShieldBreak", false);
+    private IEnumerator ShieldRestore()
+    {        
         shieldBroken = false;
         shieldBreak = false;
         shieldRestore = false;
+        while (crackAlpha > 0 && !shieldBroken)
+        {
+            crackAlpha -= .05f;
+            topCrackFade += .05f;
+            crackAlpha = Mathf.Clamp(crackAlpha, 0f, 1f);
+            topCrackFade = Mathf.Clamp(topCrackFade, 0.01f, 1f);
+            meterVFX.SetFloat("CrackAlphaClipping", crackAlpha);
+            meterVFX.SetFloat("TopLayerCrackErosion", topCrackFade);
+            yield return new WaitForSeconds(crackFadeSpeed/50f);
+        }
+        meterVFX.SetBool("ShieldBreak", false);
     }
 }
