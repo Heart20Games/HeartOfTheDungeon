34c064a6a4be935e54c2da0e5997a95f0ed95f6b
Ian Cullum <cullumi@wwu.edu>
2024-10-11 23:29:33 -0700

    Set up Callouts infrastructure; set up example of using conditionals to create a decision tree for callouts directly in ArticyX

--- Assets/Articy/MyDialogueHandler.cs
+++ Assets/Articy/MyDialogueHandler.cs
@@ -4,19 +4,99 @@ using UnityEngine;
 using Articy.Unity;
 using Articy.Unity.Interfaces;
 using TMPro;
+using System;
 
 public class MyDialogueHandler : MonoBehaviour, IArticyFlowPlayerCallbacks
 {
+    private ArticyFlowPlayer flowPlayer;
+
+    [Serializable]
+    public struct ArticyObjectWrapper
+    {
+        [SerializeField]
+        [ArticyTypeConstraint(new Type[]
+        {
+            typeof(IDialogue),
+            typeof(IFlowFragment),
+            typeof(IHub),
+            typeof(ICondition),
+            typeof(IInstruction),
+            typeof(IDialogueFragment)
+        })]
+        internal ArticyRef articyRef;
+    }
+
+    [SerializeField]
+    [ArticyTypeConstraint(new Type[]
+    {
+        typeof(IDialogue),
+        typeof(IFlowFragment),
+        typeof(IHub),
+        typeof(ICondition),
+        typeof(IInstruction),
+        typeof(IDialogueFragment)
+    })]
+    internal ArticyRef startOn;
+
+    [SerializeField] private List<ArticyObjectWrapper> changeTos = new();
+    public TMP_Text descriptionLabel;
+
+    public bool useHandlerNodes = false;
+
+    private void Awake()
+    {
+        flowPlayer = GetComponent<ArticyFlowPlayer>();
+    }
+
+    private void Start()
+    {
+        if (useHandlerNodes)
+        {
+            Callouts.main?.TriggerCallout(3);
+            flowPlayer.StartOn = startOn.GetObject<ArticyObject>();
+            StartCoroutine(ChangeToAfterSeconds(2));
+        }
+    }
+
+    public void SetTo(int idx)
+    {
+        if (idx < changeTos.Count)
+        {
+            flowPlayer.StartOn = changeTos[idx].articyRef.GetObject<ArticyObject>();
+        }
+    }
+
+    private IEnumerator ChangeToAfterSeconds(float seconds)
+    {
+        foreach (ArticyObjectWrapper changeTo in changeTos)
+        {
+            yield return new WaitForSeconds(seconds);
+            flowPlayer.StartOn = changeTo.articyRef.GetObject<ArticyObject>();
+        }
+    }
+
     public void OnBranchesUpdated(IList<Branch> aBranches)
     {
+        Debug.Log($"Branches? {aBranches.Count}");
+        if (aBranches.Count > 0)
+        {
+            StartCoroutine(ContinueAfter(0.5f));
+        }
     }
-    public TMP_Text descriptionLabel;
+
+    public IEnumerator ContinueAfter(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        flowPlayer.Play();
+    }
 
     public void OnFlowPlayerPaused(IFlowObject aObject)
     {
         var objWithText = aObject as IObjectWithLocalizableText;
+        
         if(objWithText != null)
         {
+            Debug.Log($"Line: {objWithText.Text}");
             descriptionLabel.text = objWithText.Text;
         }
     }
